d05dc5aad63386f6a823c48a081ffbf86cd40c7c
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-17 16:42:25 -0500

    [FEAT] add cards and spearate cards by status

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+type Status = 'complete' | 'progress'
+interface Data {
+  id: number
+  content: string
+  status: Status
+}
+const data: Data[] = [
+  {
+    id: 1,
+    content: 'Superman',
+    status: 'complete'
+  },
+  {
+    id: 2,
+    content: 'Flash',
+    status: 'progress'
+  }
+]
+
 const App = () => {
   return (
     <div>
       <Title />
+      <div>
+        <ContainerCards items={data as any} status='complete' />
+        <ContainerCards items={data as any} status='progress' />
+      </div>
     </div>
   )
 }
@@ -13,5 +36,31 @@ export const Title = () => {
       <h1>Creating Drag & Drop</h1>
       <span>(without external libraries)</span>
     </>
+  )
+}
+
+interface Props {
+  items: Data[]
+  status: Status
+}
+
+export const ContainerCards = ({ items = [], status }: Props) => {
+  return (
+    <div>
+      {
+        items.map(item => (
+          status === item.status && <CardItem key={item.id} {...item} />
+        ))
+      }
+    </div>
+  )
+}
+
+export const CardItem = ({ id, content, status }: Data) => {
+  return (
+    <div>
+      <p>{content}</p>
+      <span>{status}</span>
+    </div>
   )
 }
