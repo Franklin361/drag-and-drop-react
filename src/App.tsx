12eab007c6ff1e90354d1266402154b6a168cf4b
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-07-18 11:40:35 -0500

    [STYLE] add styles to compoenents

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,40 @@
-type Status = 'complete' | 'progress'
+type Status = 'good' | 'bad' | 'normal'
 interface Data {
   id: number
   content: string
   status: Status
 }
+
 const data: Data[] = [
   {
     id: 1,
-    content: 'Superman',
-    status: 'complete'
+    content: 'Aqua-man',
+    status: 'bad'
   },
   {
     id: 2,
     content: 'Flash',
-    status: 'progress'
-  }
+    status: 'good'
+  },
+  {
+    id: 3,
+    content: 'Green Lantern',
+    status: 'good'
+  },
 ]
+
+const containers: Status[] = ['good', 'normal', 'bad']
 
 const App = () => {
   return (
-    <div>
+    <div className="container-main flex">
       <Title />
-      <div>
-        <ContainerCards items={data as any} status='complete' />
-        <ContainerCards items={data as any} status='progress' />
+      <div className="grid">
+        {
+          containers.map(container => (
+            <ContainerCards items={data} status={container} key={container} />
+          ))
+        }
       </div>
     </div>
   )
@@ -32,10 +43,10 @@ export default App
 
 export const Title = () => {
   return (
-    <>
+    <div className="title flex">
       <h1>Creating Drag & Drop</h1>
-      <span>(without external libraries)</span>
-    </>
+      <span>( without external libraries )</span>
+    </div>
   )
 }
 
@@ -46,7 +57,8 @@ interface Props {
 
 export const ContainerCards = ({ items = [], status }: Props) => {
   return (
-    <div>
+    <div className="layout-cards">
+      <p>{status} hero</p>
       {
         items.map(item => (
           status === item.status && <CardItem key={item.id} {...item} />
@@ -58,9 +70,8 @@ export const ContainerCards = ({ items = [], status }: Props) => {
 
 export const CardItem = ({ id, content, status }: Data) => {
   return (
-    <div>
+    <div className="card-container">
       <p>{content}</p>
-      <span>{status}</span>
     </div>
   )
 }
